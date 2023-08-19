0e1d19cf3d2091d016367d5b340c54735f539255
leikiki <142652258+leikiki@users.noreply.github.com>
2023-08-19 14:46:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+/*
+ * @Author: example@aomain.com 1824740184@qq.com
+ * @Date: 2023-08-16 02:04:00
+ * @LastEditors: example@aomain.com 1824740184@qq.com
+ * @LastEditTime: 2023-08-19 02:50:04
+ * @FilePath: \Blog顶置卡片设计（css+js）\script.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// var swiper = new Swiper('.blog-slider', {
+//       spaceBetween: 30,
+//       effect: 'fade',
+//       loop: true,
+//       mousewheel: {
+//         invert: false,
+//       },
+//       direction: 'vertical',
+//       pagination: {
+//         el: '.blog-slider_pagination',
+//         clickable: true,
+//       },
+//       on: {
+//         slideChangeTransitionEnd: function(){
+//           var inex = this.activeIndex;
+//           console.log(inex);
+//         },
+//       },
+//     });
